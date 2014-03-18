31337e84ec81a5cb71db9da9e0527f133404fdcc
Will Leinweber <will@bitfission.com>
2014-03-18 15:57:29 -0700

    scan for simple comments

--- elite.rb
+++ elite.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
+
+
 arg = File.expand_path(ARGV[0])
 abort('usage: elite <directory|file>') unless arg
 abort("error: cannot find #{arg}") unless File.exists?(arg)
+
 files = Dir.exists?(arg) ?  Dir.glob(arg + '/**/*.rb') : [arg]
 
-p files
+files.each do |path|
+  File.open(path) do |file|
+    file.each_line do |line|
+       line if line =~ /\A\s+?#/
+    end
+  end
+end
