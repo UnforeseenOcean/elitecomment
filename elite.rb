31337bc363ee736514ac6b02dded37538d5b69a0
Will Leinweber <will@bitfission.com>
2014-03-18 16:10:06 -0700

    r3pl4c3 c0mm3ntz

--- elite.rb
+++ elite.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 require 'leetspeak'
+require 'tempfile'
+require 'fileutils'
 
 arg = File.expand_path(ARGV[0])
 abort('usage: elite <directory|file>') unless arg
@@ -8,9 +10,27 @@ abort("error: cannot find #{arg}") unless File.exists?(arg)
 files = Dir.exists?(arg) ?  Dir.glob(arg + '/**/*.rb') : [arg]
 
 files.each do |path|
-  File.open(path) do |file|
-    file.each_line do |line|
-      puts line.leet(false) if line =~ /\A\s+?#/
+  tmp = Tempfile.new('working')
+  count = 0
+  begin
+    File.open(path) do |file|
+
+      file.each_line do |line|
+        if line =~ /\A\s+?#/
+          tmp.puts line.leet(false)
+          count += 1
+        else
+          tmp.puts line
+        end
+      end
+
     end
+
+    tmp.rewind
+    FileUtils.mv(tmp.path, path)
+    puts '%4d comments: %s' % [count, path]
+  ensure
+    tmp.close
+    tmp.unlink
   end
 end
