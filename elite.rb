313378a05adeee8698aac596af511209082b73ce
Will Leinweber <will@bitfission.com>
2014-03-18 15:27:51 -0700

    arg parsing

--- elite.rb
+++ elite.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts 'hi'
+arg = File.expand_path(ARGV[0])
+abort('usage: elite <directory|file>') unless arg
+abort("error: cannot find #{arg}") unless File.exists?(arg)
+
+puts arg
